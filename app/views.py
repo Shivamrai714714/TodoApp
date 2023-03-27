5a164691d0989573bc459478e29a5441c1ccf752
Shivam rai <shivamrai714@gmail.com>
2023-03-27 20:57:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate , login as loginUser , logout
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
+# Create your views here.
+from app.forms import TODOForm
+from app.models import TODO
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='login')          #adding the decorator login_required to show home page to loggedIn user only.
+def home(request):
+    if request.user.is_authenticated:
+        user = request.user
+        form = TODOForm()
+        todos = TODO.objects.filter(user = user).order_by('priority')               #To show the list of Todo in ascending order or -priorrity in descending order.
+        
+        return render(request , 'index.html' , context={'form' : form , 'todos' : todos})
+
+
+
+def login(request):
+    if request.method == 'GET':
+        form1 = AuthenticationForm()
+        context = {
+            "form" : form1
+        }
+        return render(request , 'login.html' , context=context )
+    
+    else:
+        form = AuthenticationForm(data=request.POST)      #fething the data filled from user.
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username = username , password = password)  #inbuilt function to check the user frrom the database
+            if user is not None:
+                loginUser(request , user)             #inbuilt function to login user. Basically set the current user to the sesssion.
+                return redirect('home')
+        else:
+            context = {
+                "form" : form
+            }
+            return render(request , 'login.html' , context=context )
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context = {
+            "form" : form
+        }
+        return render(request , 'signup.html' , context=context)
+    else:
+        print(request.POST)
+        form = UserCreationForm(request.POST)  
+        context = {
+            "form" : form
+        }
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            if user is not None:
+                return redirect('login')
+        else:
+            return render(request , 'signup.html' , context=context)
+
+
+
+@login_required(login_url='login')
+def add_todo(request):
+    if request.user.is_authenticated:
+        user = request.user
+        print(user)
+        form = TODOForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            todo = form.save(commit=False)
+            todo.user = user
+            todo.save()
+            print(todo)
+            return redirect("home")
+        else: 
+            return render(request , 'index.html' , context={'form' : form})
+
+
+def delete_todo(request , id ):
+    print(id)
+    TODO.objects.get(pk = id).delete()
+    return redirect('home')
+
+def change_todo(request , id  , status):
+    todo = TODO.objects.get(pk = id)
+    todo.status = status
+    todo.save()
+    return redirect('home')
+
+
+def signout(request):
+    logout(request)
+    return redirect('login')
